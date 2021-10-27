548bd68710f4baed4b50179c38fc4b309263a1b0
Alex Sykes <alex@alexsykes.net>
2021-10-27 20:06:18 +0100

    Adding FAB, OnClickListener to TrackListActivity

--- app/src/main/java/com/alexsykes/trackmonster/activities/TrackListActivity.java
+++ app/src/main/java/com/alexsykes/trackmonster/activities/TrackListActivity.java
@@ -1,23 +1,25 @@
 package com.alexsykes.trackmonster.activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.alexsykes.trackmonster.R;
 import com.alexsykes.trackmonster.data.TrackDbHelper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -40,12 +42,28 @@ public class TrackListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name = "Sample track";
-                String description = "Something about this track";
-                trackDbHelper.addRandomTrack();
+
+                Intent intent = new Intent(getApplicationContext(), TrackDialogActivity.class);
+                // intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
             }
         });
 
+    }private void showAddItemDialog(Context c) {
+        final EditText taskEditText = new EditText(c);
+        AlertDialog dialog = new AlertDialog.Builder(c)
+                .setTitle("Add a new task")
+                .setMessage("What do you want to do next?")
+                .setView(taskEditText)
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String task = String.valueOf(taskEditText.getText());
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .create();
+        dialog.show();
     }
 
     private void populateTrackList() {
